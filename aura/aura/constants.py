09c3999ce63c1a7031b57180e1b2f069abcd60e0
Zaraius <zaraiusbillimoria@gmail.com>
2025-11-21 18:30:32 -0500

    noted down the actual max speed of motors full power

--- aura/aura/constants.py
+++ aura/aura/constants.py
@@ -11,6 +11,7 @@ DRIVE_MOTOR_GEAR_RATIO = 16/9.0 # gear ratio 16:9
 
 MAX_STEERING_ANGLE = 0.5235987756  # radians, ±30°
 # Theoretical max speed 42.56 m/s
+# TODO: REAL MEASURED FULL POWER: 1.5 mph = 0.67 m/s
 THEORETICAL_MAX_SPEED = 42.56 # MAX_DRIVE_RPM_LOADED * DRIVE_MOTOR_GEAR_RATIO * WHEEL_RADIUS * 2 * 3.14159 / 60 # m/s, max forward speed with loads
 # MAX SPEED constraints the car's linear speed, when steering, it can go up to 38 m/s currently to maintain this linear speed
 MAX_SPEED_LINEAR = 30.0 # m/s limited to max linear velocity of the car
